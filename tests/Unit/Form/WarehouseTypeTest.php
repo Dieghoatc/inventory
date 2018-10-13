c83751db8bea02991819523d963dc0d077694177
Sergio Barbosa <sbarbosa115@gmail.com>
2018-10-12 23:36:26 -0500

    Solved issue with auth. New testing framework implemented.

---
+++ tests/Unit/Form/WarehouseTypeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Unit\Form;
+
+use App\Entity\Warehouse;
+use App\Form\WarehouseType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class WarehouseTypeTest extends TypeTestCase
+{
+    public function testSomething()
+    {
+        $formData = array(
+            'name' => 'TEST-WAREHOUSE-01'
+        );
+        $objectToCompare = new Warehouse();
+        $form = $this->factory->create(WarehouseType::class, $objectToCompare);
+
+        $warehouse = new Warehouse();
+        $warehouse->setName('TEST-WAREHOUSE-01');
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($warehouse, $objectToCompare);
+    }
+}
