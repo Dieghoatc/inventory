7efa1723214dcba6cad2cc96fc2eb8d7cb05511b
Sergio Barbosa <sbarbosa115@gmail.com>
2018-11-12 08:17:33 -0500

    Solved issues with migration. Disabled register page.

--- src/Migrations/Version20181111142456.php
+++ src/Migrations/Version20181111142456.php
@@ -15,7 +15,6 @@ final class Version20181111142456 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE logs');
         $this->addSql('ALTER TABLE log ADD detail LONGTEXT DEFAULT NULL');
     }
 
@@ -23,8 +22,6 @@ final class Version20181111142456 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('CREATE TABLE logs (id INT AUTO_INCREMENT NOT NULL, detail LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE log DROP detail');
     }
 }
